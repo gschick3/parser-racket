0723672c1c92bc62a5ad3ecb3f29c7e0f334051f
gschick3 <gschick3@gmail.com>
2024-02-06 17:31:51 -0600

    create tokenizer

--- parser.rkt
+++ parser.rkt
@@ -1,33 +1,9 @@
 #lang racket
 
-(define (read-lines filename)
-  (map(lambda (line) (string-split line))
-      (file->lines filename)))
-
-(define (digit? s)
-  (regexp-match-exact? #rx"[0-9]" s))
-
-(define (numsign? s)
-  (regexp-match-exact? #rx"[+|-]?" s))
-
-(define (split-char-str s)
-  (string-split s #rx"(?<=.)(?=.)"))
-
-(define (num? s)
-  (let ([s-split (split-char-str s)])
-    (and (or (numsign? (first s-split)) (digit? (first s-split)))
-         (andmap digit? (rest s-split)))))
-
-(define (id? s)
-  (regexp-match-exact? #rx"[a-zA-Z][a-zA-Z0-9]*" s))
-
 (define (etail? lst)
   #t)
 
 (define (expr? lst)
-  #t)
-
-(define (bool-op? lst)
   #t)
 
 (define (boolean? lst)
@@ -54,5 +30,3 @@
 (define (program? lst)
   (and (linelist? (drop-right lst 1))
        (equal? "$$" (car (last lst)))))
-
-(program? (read-lines "test1.txt"))
